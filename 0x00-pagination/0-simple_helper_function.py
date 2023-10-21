42848be82eb2effd0560919f34e172c782da1aaa
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-22 00:19:15 +0100

    0-simple_helper_function.py updated

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''A module that contains a function named index_range that takes two integer'''
 
+
 def index_range(page: int, page_size: int) -> tuple:
     '''A function that return a tuple of size two containing a start index and an end index'''
     end_idx = page * page_size
