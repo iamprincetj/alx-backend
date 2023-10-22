918dea41435c7d3fd1bafc78fca8a61b781604b9
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-22 01:01:40 +0100

    1-simple_pagination.py updated

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -27,8 +27,10 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         new = []
-        assert isinstance(page, int) and page > 0, 'First argument must be an integer greater than 0'
-        assert isinstance(page_size, int) and page_size > 0, 'First argument must be an integer greater than 0'
+        assert type(page) == int
+        assert type(page_size) == int
+        assert page > 0
+        assert page_size > 0
         idx = index_range(page, page_size)
         try:
             for i in range(page_size):
