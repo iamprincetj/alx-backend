638d67802eafccd3816bc838e01bc8e1b642e558
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-22 01:14:53 +0100

    1-simple_pagination.py updated

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -26,15 +26,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        new = []
-        assert type(page) == int
-        assert type(page_size) == int
-        assert page > 0
-        assert page_size > 0
-        csv_size = len(self.dataset())
+        '''A method that takes two integer arguments '''
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page > 0
+
         start, end = index_range(page, page_size)
-        end = min(end, csv_size)
         try:
-            return self.dataset()[idx[0]:end]
+            return self.dataset()[start:end]
         except IndexError:
             return []
