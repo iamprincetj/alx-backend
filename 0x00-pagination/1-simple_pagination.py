e6ee5cd92a2b349aef7109f9140ae73ea2d1e86a
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-22 01:24:34 +0100

    1-simple_pagination.py updated

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+'''
+    Simple pagination.
+'''
 import csv
 import math
 from typing import List
@@ -38,10 +41,32 @@ class Server:
         '''
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
-        self.dataset()
+        mylist = self.dataset()
 
-        if self.dataset() is None:
+        start, end = index_range(page, page_size)
+
+        try:
+            return mylist[start:end]
+        except IndexError:
             return []
+server = Server()
 
-        indexRange = index_range(page, page_size)
-        return self.dataset()[indexRange[0]:indexRange[1]]
+try:
+    should_err = server.get_page(-10, 2)
+except AssertionError:
+    print("AssertionError raised with negative values")
+
+try:
+    should_err = server.get_page(0, 0)
+except AssertionError:
+    print("AssertionError raised with 0")
+
+try:
+    should_err = server.get_page(2, 'Bob')
+except AssertionError:
+    print("AssertionError raised when page and/or page_size are not ints")
+
+
+print(server.get_page(1, 3))
+print(server.get_page(3, 2))
+print(server.get_page(3000, 100))
