d1b159983eeee3821b3f910289a8021dee8125ce
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-31 12:11:44 +0100

    task 0 updated

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-'''Basic Flask app'''
-from flask import Flask, render_template
+"""
+Basic Flask app
+"""
 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    '''renders an index.html'''
+    """
+    hello world
+    """
     return render_template('0-index.html')
