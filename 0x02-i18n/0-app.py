09adb3a483b372f64f2276d2b483f428e50edfb4
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-31 12:05:52 +0100

    task 0 updated

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,11 +2,15 @@
 '''Basic Flask app'''
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
+    '''renders an index.html'''
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
