e4b4a986e87d9edf6cd57efd05ebe587976149b5
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-31 12:07:20 +0100

    task 0 updated

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     '''renders an index.html'''
     return render_template('0-index.html')
